1d3951d1c66cdf13ebb08995c36efd5bc8f13f06
Haseeb Malik <haseebmalik.31094@gmail.com>
2022-10-06 18:40:59 +0500

    Bootstrap & Splash Screen: * Added routing to conte-pwa. * Set up bootstrap & ng-bootstrap in pwa. * Added a splash screen.

--- apps/conte-pwa/src/app/app.module.ts
+++ apps/conte-pwa/src/app/app.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: environment.production,
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  declarations: [AppComponent, SplashScreenComponent],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    NgbModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
