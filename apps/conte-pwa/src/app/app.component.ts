fb80649a37edc7f598459e3bfc6da710ef1fee82
Haseeb Malik <haseebmalik.31094@gmail.com>
2022-10-25 14:41:00 +0500

    Reverting changes

--- apps/conte-pwa/src/app/app.component.ts
+++ apps/conte-pwa/src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { delay } from './utils/constants';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'conte-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'conte-pwa';
-  targetElement!: Element | null;
-
-  constructor(private router: Router){}
-
-  ngOnInit() {
-    this.targetElement = document.querySelector('html');
-  }
-
-  async refreshPage() {
-    await delay(1000);
-    this.router.navigate([''])
-  }
 }
