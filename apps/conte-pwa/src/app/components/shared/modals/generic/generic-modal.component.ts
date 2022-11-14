eae41b0cfcc2a790e2ae5a8580f29e878651a73a
Haseeb Malik <haseebmalik.31094@gmail.com>
2022-11-14 19:23:56 +0500

    Changes in UI: * Updated generic model. * Used generic model for task feedback in treatment plan. * Updated CSS for treatment plan and generic. * reverted toast component

--- apps/conte-pwa/src/app/components/shared/modals/generic/generic-modal.component.ts
+++ apps/conte-pwa/src/app/components/shared/modals/generic/generic-modal.component.ts
@@ -1,26 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { taskFeedback } from 'apps/conte-pwa/src/app/models/treatmentplan';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { delay } from 'apps/conte-pwa/src/app/utils/constants';
+import { ToastService } from 'apps/conte-pwa/src/app/services/toast.service';
 
 @Component({
   selector: 'conte-generic-modal',
   templateUrl: './generic-modal.component.html',
   styleUrls: ['./generic-modal.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate(600)]),
+      transition(':leave', animate(600, style({ opacity: 0 }))),
+    ]),
+  ],
 })
 export class GenericModalComponent implements OnInit {
   @Input() heading = '';
+  @Input() subHeading = '';
   @Input() logo = '';
   @Input() body = '';
   @Input() list = [];
   @Input() listActionText = '';
   @Input() listActionLogo = '';
+  @Input() questionAnswers: taskFeedback[] = [];
+  @Input() QAbuttonText = '';
+  @Input() QAbuttonLogo = '';
   @Input() listAction!: (args: any) => void;
   @Input() buttonText = '';
   @Input() buttonLoadingText = '';
-  @Input() buttonAction!: () => void;
+  @Input() buttonAction!: (args: any) => void;
   buttonState = 'static';
+  secButtonState = 'static';
 
-  constructor(private activeModal: NgbActiveModal) {}
+  constructor(private activeModal: NgbActiveModal, private toast: ToastService) {}
 
   ngOnInit(): void {}
 
@@ -29,6 +44,24 @@ export class GenericModalComponent implements OnInit {
       this.buttonState = 'loading';
       await delay(1500);
     }
+    if (this.questionAnswers.length) {
+      for (const question of this.questionAnswers) {
+        if (!question.answer) {
+          this.toast.show('Please answer every question first.', { classname: 'bg-danger text-light', icon: 'error' });
+          this.buttonState = 'static';
+          return;
+        }
+      }
+      this.buttonAction(this.questionAnswers);
+    }
     this.activeModal.close();
+  }
+
+  addQuestion() {
+    this.questionAnswers.push({ question: '', answer: '' });
+  }
+
+  removeQuestion(index: number) {
+    this.questionAnswers.splice(index, 1);
   }
 }
