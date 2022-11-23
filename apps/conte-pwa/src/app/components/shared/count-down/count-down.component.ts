8f3db043e22456f10b3eabba91a3583eae4570f9
Haseeb Malik <haseebmalik.31094@gmail.com>
2022-11-23 16:05:02 +0500

    * Updated timer for real time ending

--- apps/conte-pwa/src/app/components/shared/count-down/count-down.component.ts
+++ apps/conte-pwa/src/app/components/shared/count-down/count-down.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
 import { SpinnerService } from '../../../services/spinner.service';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { delay } from '../../../utils/constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'conte-count-down',
   templateUrl: './count-down.component.html',
   styleUrls: ['./count-down.component.scss'],
+  animations: [trigger('fade', [transition(':enter', [style({ opacity: 0 }), animate(760)])])],
 })
 export class CountDownComponent implements OnInit, OnDestroy {
   private subscription!: Subscription;
@@ -16,21 +20,35 @@ export class CountDownComponent implements OnInit, OnDestroy {
   hoursInADay = 24;
   minutesInAnHour = 60;
   SecondsInAMinute = 60;
+  showTimer!: boolean;
 
-  timeDifference: any;
-  secondsToDday: any;
-  minutesToDday: any;
-  hoursToDday: any;
-  daysToDday: any;
+  timeDifference!: number;
+  secondsToDday!: number;
+  minutesToDday!: number;
+  hoursToDday!: number;
+  daysToDday!: number;
 
-  constructor(private spinner: SpinnerService) {}
+  constructor(private spinner: SpinnerService, private router: Router) {}
+
+  async ngOnInit() {
+    this.subscription = interval(1000).subscribe((x) => {
+      this.getTimeDifference();
+      if (this.timeDifference <= 0) this.showTimer = false;
+    });
+
+    await delay(2000);
+    if (this.timeDifference <= 0) this.showTimer = false;
+    else this.showTimer = true;
+
+    this.spinner.hide();
+  }
 
   private getTimeDifference() {
     this.timeDifference = this.dDay.getTime() - new Date().getTime();
     this.allocateTimeUnits(this.timeDifference);
   }
 
-  private allocateTimeUnits(timeDifference: any) {
+  private allocateTimeUnits(timeDifference: number) {
     this.secondsToDday = Math.floor((timeDifference / this.milliSecondsInASecond) % this.SecondsInAMinute);
     this.minutesToDday = Math.floor(
       (timeDifference / (this.milliSecondsInASecond * this.minutesInAnHour)) % this.SecondsInAMinute
@@ -43,11 +61,10 @@ export class CountDownComponent implements OnInit, OnDestroy {
     );
   }
 
-  ngOnInit() {
-    this.subscription = interval().subscribe((x) => {
-      this.getTimeDifference();
+  navToDashboard() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['dashboard']);
     });
-    this.spinner.hide();
   }
 
   ngOnDestroy() {
