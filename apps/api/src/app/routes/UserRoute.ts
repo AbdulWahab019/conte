8687b92ca17fe98f14a7d45be421fd8780c916da
Abdul Wahab <50889251+AbdulWahab019@users.noreply.github.com>
2022-11-24 13:34:00 +0500

    * api to get all users for web * added is_subscribed to users table * updated webhook listener to update is_subscribed on basis of user subscription

--- apps/api/src/app/routes/UserRoute.ts
+++ apps/api/src/app/routes/UserRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { acceptTermsOfUse, getUserProfile, watchOrientationVideo } from '../controllers/UserController';
+import { acceptTermsOfUse, getAllUsers, getUserProfile, watchOrientationVideo } from '../controllers/UserController';
 import { authorize } from '../middlewares/auth';
 
 const router = Router();
@@ -10,5 +10,8 @@ router.get('/profile', authorize, getUserProfile);
 router.put('/accept-terms-of-use', authorize, acceptTermsOfUse);
 
 router.put('/watch-orientation-video', authorize, watchOrientationVideo);
+
+// TODO - update middleware to autorize web user
+router.get('/', getAllUsers);
 
 export default router;
