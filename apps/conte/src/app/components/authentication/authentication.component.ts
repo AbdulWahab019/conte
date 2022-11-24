de118c11170b61352124627e0583ee2f1b4f7616
Haseeb Malik <haseebmalik.31094@gmail.com>
2022-11-24 20:17:32 +0500

    haseeb/changes: * UI and css updated

--- apps/conte/src/app/components/authentication/authentication.component.ts
+++ apps/conte/src/app/components/authentication/authentication.component.ts
@@ -1,60 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Router } from '@angular/router';
-import { FormBuilder, FormControl,FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastService } from '../../Shared/services/toast.service';
 
 @Component({
   selector: 'conte-dashboard',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.scss'],
-  animations: [trigger('fade', [transition(':enter', [style({ opacity: 0 }), animate(760)])])],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate(600)]),
+      transition(':leave', animate(600, style({ opacity: 0 }))),
+    ]),
+    trigger('fade', [transition(':enter', [style({ opacity: 0 }), animate(760)])]),
+  ],
 })
 export class AuthenticationComponent implements OnInit {
+  authenticationForm: FormGroup = {} as FormGroup;
+  formType = 'login';
+
   email = '';
   password = '';
   confirm_password = '';
   buttonState = 'static';
-  loginState = true;
-  registerState = false;
-  stayLoggedInCheck = true;
-  termsOfUseScreen = false;
-  signinForm: FormGroup = {} as FormGroup;
-  registrationForm: FormGroup = {} as FormGroup;
 
-  constructor(
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private toast: ToastService
-  ) {}
+  constructor(private router: Router, private formBuilder: FormBuilder, private toast: ToastService) {}
 
   ngOnInit(): void {
-    const token = localStorage.getItem('token');
-    if (token) {
-      this.router.navigate(['']);
-    }
-
-    this.signinForm = this.formBuilder.group({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    });
-
-    this.registrationForm = this.formBuilder.group(
-      {
+    if (this.formType === 'login') {
+      this.authenticationForm = this.formBuilder.group({
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-        confirm_password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-      },
-      { validators: this.checkPasswords }
-    );
+      });
+    }
   }
 
   get f(): any {
-    return this.signinForm.controls;
-  }
-
-  get f2(): any {
-    return this.registrationForm.controls;
+    return this.authenticationForm.controls;
   }
 
   checkPasswords(form: FormGroup) {
@@ -62,12 +46,22 @@ export class AuthenticationComponent implements OnInit {
   }
 
   switchForm() {
-    if (this.loginState) {
-      this.loginState = false;
-      this.registerState = true;
+    if (this.formType === 'login') {
+      this.formType = 'register';
+      this.authenticationForm = this.formBuilder.group(
+        {
+          email: new FormControl('', [Validators.required, Validators.email]),
+          password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+          confirm_password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        },
+        { validators: this.checkPasswords }
+      );
     } else {
-      this.registerState = false;
-      this.loginState = true;
+      this.formType = 'login';
+      this.authenticationForm = this.formBuilder.group({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      });
     }
   }
 
@@ -78,27 +72,25 @@ export class AuthenticationComponent implements OnInit {
       password: this.f.password.value,
     };
     console.log(credentials);
-        localStorage.setItem('token', '1234567890');
-        localStorage.setItem('user_email', 'test@test.com');
+    localStorage.setItem('token', '1234567890');
+    localStorage.setItem('user_email', 'test@test.com');
 
-          this.router.navigate(['home']);
-
+    this.router.navigate(['home']);
   }
 
   register() {
     this.buttonState = 'loading';
     const credentials = {
-      email: this.f2.email.value,
-      password: this.f2.password.value,
-      confirm_password: this.f2.confirm_password.value,
+      email: this.f.email.value,
+      password: this.f.password.value,
+      confirm_password: this.f.confirm_password.value,
     };
-        localStorage.setItem('token', '12345');
-        localStorage.setItem('user_email', 'test@test.com');
-        this.buttonState = 'static';
+    localStorage.setItem('token', '12345');
+    localStorage.setItem('user_email', 'test@test.com');
+    this.buttonState = 'static';
 
-        this.toast.show('Signed up successfully.', { classname: 'bg-success text-light', icon: 'success' });
+    this.toast.show('Signed up successfully.', { classname: 'bg-success text-light', icon: 'success' });
 
-        this.router.navigate(['home']);
-    
+    this.router.navigate(['home']);
   }
 }
