00d3dca21cc835e53a84f345ff8165858e57cd0f
Abdul Wahab <50889251+AbdulWahab019@users.noreply.github.com>
2022-11-28 18:45:23 +0500

    * interfaces for Api req and res for user routes

--- libs/models/src/lib/users/user.model.ts
+++ libs/models/src/lib/users/user.model.ts
@@ -22,3 +22,21 @@ export interface UpdateUserResponse {
   zip_code: string;
   estimated_max_velocity: number;
 }
+
+export interface GetUserDataApiRes {
+  first_name: string;
+  last_name: string;
+  cell_phone: string;
+  birth_date: string;
+  address: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  email: string;
+  estimated_max_velocity: number;
+  is_terms_of_use_accepted: boolean;
+  is_orientation_video_watched: boolean;
+  is_subscribed: boolean;
+  num_skipped_tasks: number;
+  num_completed_tasks: number;
+}
