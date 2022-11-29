24e549cbbdcf5c6d14a572e72516936844476153
Abdul Wahab <50889251+AbdulWahab019@users.noreply.github.com>
2022-11-29 17:14:12 +0500

    * refactoring

--- apps/api/src/app/controllers/TreatmentPlanController.ts
+++ apps/api/src/app/controllers/TreatmentPlanController.ts
@@ -36,8 +36,7 @@ export async function uploadTreatmentPlan(req: Request, res: Response) {
 }
 
 export async function getTasksByDate(req: Request, res: Response) {
-  // const { id: user_id } = req['user'];
-  const user_id = 59;
+  const { id: user_id } = req['user'];
   const { date } = req.params;
 
   const tasks = await getUserTasksByDate(user_id, date);
