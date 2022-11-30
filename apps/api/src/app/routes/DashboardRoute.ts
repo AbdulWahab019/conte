f5c146b2fc2e944b1f88ada4777555181f4d8186
Abdul Wahab <50889251+AbdulWahab019@users.noreply.github.com>
2022-11-30 21:42:02 +0500

    * refactor

--- apps/api/src/app/routes/DashboardRoute.ts
+++ apps/api/src/app/routes/DashboardRoute.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { getDashboardData, getUserTasksCalender } from '../controllers/DashboardController';
+import { getDashboardData, getUserTasksCalendar } from '../controllers/DashboardController';
 import { authorize } from '../middlewares/auth';
 
 const router = Router();
 
 router.get('/', authorize, getDashboardData);
 
-router.get('/calender/date/:date', authorize, getUserTasksCalender);
+router.get('/calendar/date/:date', authorize, getUserTasksCalendar);
 
 export default router;
