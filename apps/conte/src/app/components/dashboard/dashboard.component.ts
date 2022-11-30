2e704c70e6d9ccef7e391d74fb6faa5a0407d659
Hamza Ijaz <iamhamzaijaz11@gmail.com>
2022-11-30 19:51:17 +0500

    * header set

--- apps/conte/src/app/components/dashboard/dashboard.component.ts
+++ apps/conte/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../Shared/services/user.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { UserService } from '../../Shared/services/user.service';
 })
 export class DashboardComponent implements OnInit {
   url = '';
-  allUsers : any = [];
+  allUsers: any = [];
 
   orderTableHeaders: any = [
     { title: 'id', value: 'id', sort: true, orderBy: '' },
@@ -20,18 +20,25 @@ export class DashboardComponent implements OnInit {
       orderBy: '',
     },
     { title: 'estimated_max_velocity', value: 'estimated_max_velocity', sort: true, orderBy: '' },
-    { title: 'email', value: 'email', sort: true, orderBy: '' }
+    { title: 'email', value: 'email', sort: true, orderBy: '' },
   ];
-  constructor(private router: Router,private UsersService : UserService) {}
+  constructor(private router: Router, private UsersService: UserService) {}
   ngOnInit(): void {
-    this.UsersService.getAllUsers().then((resp)=>{
-      
+    this.UsersService.getAllUsers().then((resp) => {
       this.allUsers = resp.data.users.map((order: any) => ({
-            id: order.id,
-            Name: `${order.last_name}, ${order.first_name}`,
-            estimated_max_velocity: order.estimated_max_velocity,
-            email: order.email,
-          }));
-    })
-}
+        id: order.id,
+        Name: `${order.last_name}, ${order.first_name}`,
+        estimated_max_velocity: order.estimated_max_velocity,
+        email: order.email,
+      }));
+    });
+  }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['']);
+    // this.spinner.show();
+    // await delay(1000);
+    // this.spinner.hide();
+    // this.toast.show('Logged out successfully.', { classname: 'bg-success text-light', icon: 'success' });
+  }
 }
